03fb13559b26397456424310f2716268d947faea
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2026-04-08 01:31:08 +0900

    chore: remove tsuno crates

--- crates/tsuno-macros/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro::TokenStream;
-
-use quote::quote;
-
-#[proc_macro_attribute]
-pub fn verify(_attr: TokenStream, _item: TokenStream) -> TokenStream {
-    quote!(compile_error!("use `//@ verify` on the line immediately before the function");).into()
-}
